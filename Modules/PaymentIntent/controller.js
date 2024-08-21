0093edbbe914f9ac7e12d1f4332c051279d13def
TaurusPun <tauruspun54@gmail.com>
2024-08-21 17:15:19 +0800

    v1.2.8

--- Modules/PaymentIntent/controller.js
+++ Modules/PaymentIntent/controller.js
@@ -1,8 +1,14 @@
+/* eslint-disable no-undef */
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
+require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const endpointSecret = process.env.STRIPE_ENDPOINT_SECRET;
+
 const validator = require('../../lib/Validator/validator');
 
 // database
+const User = require('../User/schema');
 const UserDoc = require('../UserDocs/schema');
 const DocumentType = require('../DocumentTypes/schema');
 
@@ -11,34 +17,51 @@ const ClientError = require('../../lib/Error/HttpErrors/ClientError/ClientErrors
 const ServerError = require('../../lib/Error/HttpErrors/ServerError/ServerErrors');
 
 const createPaymentIntent = async (req) => {
-  const { id } = req.user;
+  const { id, email, StripeCustomerId } = req.user;
   const { docId } = req.params;
   if (!docId) return new ClientError.NotFoundError('Not such user doc');
 
   const userdocData = await UserDoc.findById(docId);
   if (!userdocData) return new ClientError.NotFoundError('Not such user doc');
+
   if (userdocData.UserId.toString() !== id) return new ClientError.ForbiddenError('no access right');
 
-  // let errorArray = [];
-  // const { DocType, payload } = userdocData;
-  // if (!DocType || !payload) return new ClientError.BadRequestError('This doc is invalid');
-  // const { userAnswers } = payload;
-  // const docData = await DocumentType.findOne({ _id: DocType, active: true }).populate('questions');
-  // const questions = docData.questions;
-  // questions.sort((a, b) => a.order - b.order);
-  // console.log(`There are total ${questions.length} questions`);
-  // questions.forEach((question, i) => {
-  //   //console.log(question.shape);
-  //   const schema = question.shape;
-  //   const data = userAnswers[i];
-  //   const result = validator.answerValidator(schema, data, i);
-  //   if (result) errorArray.push(result);
-  // });
-  // console.log(errorArray);
   const errorArray = await validator.answerValidator(userdocData);
-  // console.log(errorArray);
-  if (errorArray.length === 0) return { message: 'All answer valid' };
-  return new ClientError.BadRequestError(`Answers of no. ${errorArray.join(', ')} are invalid`);
+  if (errorArray.length !== 0) return new ClientError.BadRequestError(`Answers of no. ${errorArray.join(', ')} are invalid`);
+
+  const documentType = await DocumentType.findById(userdocData.DocType, { active: true });
+  if (!documentType) return new ClientError.BadRequestError('This document are not valid');
+
+  let customer = {};
+  if (StripeCustomerId === '' || !StripeCustomerId) {
+    customer = await stripe.customers.create({
+      email: email,
+    });
+    const user = await User.findByIdAndUpdate(id, { StripeCustomerId: customer.id });
+    return user;
+  }
+  return { message: `user with customerId: ${StripeCustomerId} started a payment intent` };
+  //const customerId = customer.id ?? StripeCustomerId;
+  // const ephemeralKey = await stripe.ephemeralKeys.create(
+  //   {customer: customerId},
+  //   {apiVersion: '2024-06-20'}
+  // );
+  // const paymentIntent = await stripe.paymentIntents.create({
+  //   amount: documentType.price,
+  //   currency: documentType.currency,
+  //   customer: customerId,
+  //   metadata: {
+  //     userDocId: docId
+  //   },
+  // });
+  // if (!paymentIntent || !ephemeralKey || !customerId) return new ServerError.InternalServerError('Cannot start a payment intent');
+  // return {
+  //   data: {
+  //     paymentIntentClientSecret: paymentIntent.client_secret,
+  //     customerEphemeralKeySecret: ephemeralKey.secret,
+  //     customerId
+  //   }
+  // }
 };
 
 module.exports = {
